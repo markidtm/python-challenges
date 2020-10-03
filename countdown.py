2b115196e894120178df2c8e97437203d34c21fa
Jamesbond8002 <72294116+Jamesbond8002@users.noreply.github.com>
2020-10-03 17:35:38 +0530

    Update countdown.py
    Made The Code Cleaner, Professional and Easy to Understand And Also Removed An Error.

--- countdown.py
+++ countdown.py
@@ -1,22 +1,28 @@
+# All Imports:
 import datetime
+
+# Printing Current Time:
 now = datetime.datetime.now()
-print "-" * 25
-print now
-print now.year
-print now.month
-print now.day
-print now.hour
-print now.minute
-print now.second
+print ("-" * 25)
+print (now)
+print (now.year)
+print (now.month)
+print (now.day)
+print (now.hour)
+print (now.minute)
+print (now.second)
 
-print "-" * 25
-print "1 week ago was it: ", now - datetime.timedelta(weeks=1)
-print "100 days ago was: ", now - datetime.timedelta(days=100)
-print "1 week from now is it: ",  now + datetime.timedelta(weeks=1)
-print "In 1000 days from now is it: ", now + datetime.timedelta(days=1000)
+# Printing The Time Before:
+print ("-" * 25)
+print ("1 week ago was it: ", now - datetime.timedelta(weeks=1))
+print ("100 days ago was: ", now - datetime.timedelta(days=100))
+print ("1 week from now is it: ",  now + datetime.timedelta(weeks=1))
+print ("In 1000 days from now is it: ", now + datetime.timedelta(days=1000))
 
-print "-" * 25
-birthday = datetime.datetime(2012,11,04)
+# Setting The Birthday:
+birthday = datetime.datetime(2012,11,4)
 
-print "Birthday in ... ", birthday - now
-print "-" * 25
+#Printing If The Given Birthday is Now:
+print ("-" * 25)
+print ("Birthday in ... ", birthday - now)
+print ("-" * 25)
